6a61c99d8f7c10e8e921a69f322a139e4981bcdb
Guy Marshall <guymarshall9@gmail.com>
2022-07-03 16:12:26 +0100

    Add missing semicolons

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 6969;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
